fa21a06af20d5188f9efa3895daeca529cbf9900
yogesh-grl <yogesh@graniteriverlabs.in>
2022-12-04 09:47:02 +0530

    Event and Delegate Example

---
+++ CSharp/CSharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using CSharp;
+
+Example_Event();
+
+ExampleDelegates();
+
+static void Example_Event()
+{
+    //Event Example
+    EventPublisher objEvent = new EventPublisher();
+
+    //Default Event - without data (Empty Args)
+    EventSubClass1 subscriber1 = new EventSubClass1();
+    objEvent.EventSample += subscriber1.SubClassMethod1;
+
+    //Event with data 
+    CustomEventClass subscriber2 = new CustomEventClass();
+    objEvent.CustomEventSample += subscriber2.CustomMethod1;
+
+    //Publisher call 
+    EventSubscriberData ObjSubscriberData = new EventSubscriberData("Yogesh");
+    objEvent.PublisherMethod(ObjSubscriberData);
+}
+
+static void ExampleDelegates()
+{
+    //Delegate Example 
+    DelegateExample objDel = new DelegateExample();
+}
