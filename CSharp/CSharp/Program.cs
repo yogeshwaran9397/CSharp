422f2e40edaec8a6c82cae6cad6061eb22f42eb8
yogesh-grl <yogesh@graniteriverlabs.in>
2022-12-05 15:11:45 +0530

    System.Timer

--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -4,6 +4,17 @@ using System.Text;
 
 if (true)
 {
+    #region Timer 
+
+    TimerExample objtimer = new TimerExample();
+
+    Thread.Sleep(10000);
+
+    #endregion
+}
+
+if (false)
+{
     #region Generics 
 
     Generics<string> objGenericsString = new Generics<string>("Yogesh");
